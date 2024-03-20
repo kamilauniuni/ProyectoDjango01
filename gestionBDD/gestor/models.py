50b2f5c49062473da495be13a57a764527ace96b
kamila <kamilauniuni@gmail.com>
2024-03-19 21:36:05 -0500

    se agrego la bs

---
+++ gestionBDD/gestor/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class cliente(models.Model):
+    nombre=models.CharField(max_length=30)
+    direccion=models.CharField(max_length=50)
+    email=models.CharField(max_length=30)
+    telefono=models.CharField(max_length=10)
+
+class articulo(models.Model):
+    nombre=models.CharField(max_length=30)
+    seccion=models.CharField(max_length=20)
+    precio=models.IntegerField()
+    
+class pedido(models.Model):
+    numero=models.IntegerField()
+    fecha=models.DateField()
+    entregado=models.BooleanField()
